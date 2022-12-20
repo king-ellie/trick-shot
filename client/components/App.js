810167885f92d9298328e61a75c26f856e5c292e
Ellie King <ellie.king@ripplematch.com>
2022-12-19 23:09:17 -0500

    create LandingPage component

--- client/components/App.js
+++ client/components/App.js
@@ -1,23 +1,12 @@
 import React, { useState } from 'react';
 import { HashRouter as Router, Route, Link, Switch} from 'react-router-dom'
+import LandingPage from './LandingPage'
 
 function App() {
 
     return (
         <div>
-            <h1>Trick Shot</h1>
-
-            <video
-                width="1020"
-                height="630"
-                type="video/mov"
-                src="../../public/trick_shot.MOV"
-                autoPlay
-            >
-                Your browser does not support the video tag.
-            </video>
-
-            <h4>See this video on other platforms:</h4>
+            <LandingPage></LandingPage>
         </div>
     )
 }
---
+++ client/components/LandingPage.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import { HashRouter as Router, Route, Link, Switch} from 'react-router-dom'
+
+function LandingPage() {
+    return (
+        <div>
+            <h1>Trick Shot</h1>
+
+            <video
+                width="1020"
+                height="630"
+                type="video/mov"
+                src="../../public/trick_shot.MOV"
+                autoPlay
+            >
+                Your browser does not support the video tag.
+            </video>
+
+            <h4>See this video on other platforms:</h4>
+        </div>
+    )
+}
+
+export default LandingPage
