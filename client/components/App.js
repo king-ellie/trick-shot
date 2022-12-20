8387b52c3ddd102d8bb8a0b6e3a2efb26bf1cafb
Ellie King <ellie.king@ripplematch.com>
2022-12-19 23:00:11 -0500

    add video to assets + App

--- client/components/App.js
+++ client/components/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { HashRouter as Router, Route, Link, Switch} from 'react-router-dom'
 
 function App() {
+
     return (
         <div>
-            <h1>React Component has rendered</h1>
+            <h1>Trick Shot</h1>
+
+            <video
+                width="1020"
+                height="630"
+                type="video/mov"
+                src="../../public/trick_shot.MOV"
+                autoPlay
+            >
+                Your browser does not support the video tag.
+            </video>
+
+            <h4>See this video on other platforms:</h4>
         </div>
     )
 }
