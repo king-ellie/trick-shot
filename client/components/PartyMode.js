e13765983366f0ebc03c014ea821ad3398c51387
Ellie King <ellie.king@ripplematch.com>
2022-12-23 13:22:07 -0500

    add basic video display on click in PartyMode

--- client/components/PartyMode.js
+++ client/components/PartyMode.js
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function PartyMode() {
+  const [ clickCoordinates, setClickCoordinates ] = useState([])
+
+  const captureClickCoordinates = (event) => {
+    console.log(`${event.nativeEvent}`)
+    setClickCoordinates([...clickCoordinates, {
+      x: event.nativeEvent.clientX,
+      y: event.nativeEvent.clientY
+    }])
+  }
+
+
   return (
-    <div>
-      <h2>Click anywhere to party</h2>
+    <div onClick={captureClickCoordinates} className="party-mode">
+      <h2>Click anywhere below to party. The more you click, the more you party</h2>
+
+      { clickCoordinates.map((clickCoordinates) =>
+        <video
+            height="630"
+            type="video/mov"
+            src="../../public/trick_shot.MOV"
+            autoPlay
+            loop
+            style={{position: "absolute", left: clickCoordinates.x, top: clickCoordinates.y}}
+        >
+            Your browser does not support the video tag.
+        </video>
+
+      )}
     </div>
   )
 }
+
 
 export default PartyMode
