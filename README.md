3006f488bc54f6ed012ff58959b13b74cf16b5a4
king-ellie <elliebking1@gmail.com>
2025-04-02 20:35:08 -0400

    small cleanups

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 This is an inside joke. Yes, it's supposed to look like that.
 
-+ "npm i"
-+ "npm run start:backend" to start the BE web server
-+ "npm run start:frontend" to prompt the webpack build
-+ Enjoy at localhost://3000.
+- "npm i"
+- "npm run start:backend" to start the BE web server
+- "npm run start:frontend" to prompt the webpack build
+- Enjoy at localhost:3000.
--- client/components/DifferentAnglesPage.js
+++ client/components/DifferentAnglesPage.js
@@ -6,12 +6,9 @@ function DifferentAnglesPage() {
   return (
     <div>
       <h2>Have a look:</h2>
-
-      {
-        videoAngleStyles ?
-        videoAngleStyles.map((angle, idx) => <VideoAngle key={idx} angle={angle}/>) :
-          <h1>no other angles :(</h1>
-      }
+        {
+          videoAngleStyles.map((angle, idx) => <VideoAngle key={idx} angle={angle}/>) 
+        }
     </div>
   )
 }
